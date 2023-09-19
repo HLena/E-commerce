a945cb4182cbfd8353b039c063c9d4948d279e5f
HLena <gordillo.vi20@gmail.com>
2023-09-18 21:38:22 -0500

    feat: :sparkles: Add form validation for checkout

--- src/cart/components/CheckoutForm.jsx
+++ src/cart/components/CheckoutForm.jsx
@@ -1,86 +1,144 @@
-import { Button, Form } from "react-bootstrap"
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Button, FormGroup, FormControl, FormLabel } from 'react-bootstrap';
+import * as yup from 'yup';
 
-export const CheckoutForm = ({
-    handleSubmit,
-    validated,
-    emailIsValid,
-    name,
-    lastname,
-    email,
-    confirmEmail,
-    phone,
-    onInputChange
-}) => {
+export const CheckoutForm = () => {
+
+
+  const schema = yup.object().shape({
+    firstName: yup.string().required("Sus nombres son requeridos"),
+    lastName: yup.string().required("Sus apellidos son requeridos"),
+    email: yup.string().email('Correo electrónico inválido').required('El correo electrónico es requerido'),
+    confirmEmail: yup.string().required("Debe confirmar su email"),
+    phone: yup.number().required("El teléfono es requerido"),
+  });
+
+  const validateEmail = (value) => {
+    let error;
+    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)) {
+      error = 'Dirección de correo electrónico no válida';
+    }
+    return error;
+  }
+
+  const confirmEmail = (values) => {
+    let error;
+    if (values.email != values.confirmEmail) {
+      error = 'Los correos no coinciden';
+    } 
+    return error;
+  }
+
   return (
     <div className="item-container__wrapper">
-        <h4>Contacto</h4>
-        <Form noValidate validated={validated} onSubmit={(e) => handleSubmit(e)}>
-          <Form.Group className="mb-3" controlId="name">
-            <Form.Label>Nombres </Form.Label>
-            <Form.Control 
-              type="text" 
-              required 
-              minLength="3" 
-              maxLength="30" 
-              name="name" 
-              value={name} 
-              onChange={(e) => onInputChange(e)}
-            />
-            <Form.Control.Feedback type="invalid">Ingrese su nombre</Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Apellidos </Form.Label>
-            <Form.Control 
-              type="text" 
-              required 
-              minLength="3" 
-              maxLength="30" 
-              name="lastname" 
-              value={lastname} 
-              onChange={(e) => onInputChange(e)}
-            />
-            <Form.Control.Feedback type="invalid">Ingrese su apellido</Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="email">
-            <Form.Label>Email </Form.Label>
-            <Form.Control 
-              type="email" 
-              required 
-              maxLength="50" 
-              name="email" 
-              value={email} 
-              onChange={(e) => onInputChange(e)}
-            />
-            <Form.Control.Feedback type="invalid">Ingrese un email válido</Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="confirmEmail">
-            <Form.Label>Confirmar Email </Form.Label>
-            <Form.Control 
-              type="email" 
-              required 
-              maxLength="50" 
-              name="confirmEmail" 
-              value={confirmEmail} 
-              onChange={(e) => onInputChange(e)}
-              isInvalid = {!emailIsValid}
-            />
-            <Form.Control.Feedback type="invalid"> Los email no coinciden</Form.Control.Feedback>
-          </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>Teléfono </Form.Label>
-            <Form.Control 
-              type="number" 
-              required 
-              minLength="9" 
-              maxLength="20" 
-              name="phone" 
-              value={phone} 
-              onChange={(e) => onInputChange(e)}
-            />
-            <Form.Control.Feedback type="invalid">Ingrese un N° de teléfono válido</Form.Control.Feedback>
-          </Form.Group>
-          <Button type="submit">Continuar con el Pago</Button>
-        </Form>
-      </div>
-  )
+      <h4>Contacto</h4>
+      <Formik
+        validationSchema={schema}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+          confirmEmail: '',
+          phone: '',
+        }}
+      >
+        {({ handleSubmit, handleChange, values, touched, errors }) => (
+          <Form onSubmit={handleSubmit}>
+              <FormGroup className="mb-3" >
+                <FormLabel>Nombres</FormLabel>
+                <Field
+                  type="text"
+                  name="firstName"
+                  value={values.firstName}
+                  as={FormControl}
+                  onChange={handleChange}
+                  isValid={touched.firstName && !errors.firstName}
+                  isInvalid={!!errors.firstName}
+                />
+                <ErrorMessage 
+                  name='firstName'
+                  component="div"
+                  className='text-danger'
+                />
+              </FormGroup>
+              <FormGroup className="mb-3" >
+                <FormLabel>Apellidos</FormLabel>
+                <Field
+                  type="text"
+                  name="lastName"
+                  value={values.lastName}
+                  as={FormControl}
+                  onChange={handleChange}
+                  isValid={touched.lastName && !errors.lastName}
+                  isInvalid={!!errors.lastName}
+                />
+                <ErrorMessage 
+                  name='lastName'
+                  component="div"
+                  className='text-danger'
+                />
+              </FormGroup>
+              <FormGroup className="mb-3">
+                <FormLabel>Correo Electrónico</FormLabel>
+                <Field
+                  type="text"
+                  name="email"
+                  value={values.email}
+                  onChange={handleChange}
+                  validate = {validateEmail}
+                  as={FormControl}
+                  isValid={touched.email && !errors.email}
+                  isInvalid={!!errors.email}
+                  />
+                <ErrorMessage 
+                  name='email'
+                  component="div"
+                  className='text-danger'
+                  />
+              </FormGroup>
+              <FormGroup className="mb-3">
+                <FormLabel>Confirmar Correo</FormLabel>
+                <Field
+                  type="text"
+                  name="confirmEmail"
+                  value={values.confirmEmail}
+                  validate={() => confirmEmail(values)}
+                  as={FormControl}
+                  onChange={handleChange}
+                  isValid={touched.confirmEmail && !errors.confirmEmail}
+                  isInvalid={!!errors.confirmEmail}
+                />
+
+                <ErrorMessage
+                  name='confirmEmail'
+                  component="div"
+                  className='text-danger'
+                  />
+              </FormGroup>
+              <FormGroup className="mb-3">
+                <FormLabel>Teléfono</FormLabel>
+                <Field
+                  type="number"
+                  name="phone"
+                  value={values.phone}
+                  as={FormControl}
+                  onChange={handleChange}
+                  isValid={touched.phone && !errors.phone}
+                  isInvalid={!!errors.phone}
+                />
+                <ErrorMessage
+                  name='phone'
+                  component="div"
+                  className='text-danger'
+                  />
+              </FormGroup>
+              <Button type="submit">Continuar con el Pago</Button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
