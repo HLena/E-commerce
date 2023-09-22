76178044a647ac7ecbe735c46d4299c316920580
HLena <gordillo.vi20@gmail.com>
2023-09-22 10:22:11 -0500

    Create modal component for login and register form

---
+++ src/components/Modal.jsx
@@ -0,0 +1,18 @@
+import Modal from 'react-bootstrap/Modal';
+
+export const ModalLy = ({show, handleClose, children, form}) => {
+
+
+  return (
+      <Modal show={show} onHide={handleClose} >
+        <Modal.Header closeButton>
+          <Modal.Title>
+            { form == 'login' ? 'Acceso': 'Crea una nueva cuenta'}
+          </Modal.Title>
+        </Modal.Header>         
+        <Modal.Body>
+          {children}
+        </Modal.Body>
+      </Modal>
+  );
+}
